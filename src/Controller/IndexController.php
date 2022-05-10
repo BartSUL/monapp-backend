7561e0f07968753b42dc47e6d49ecf9380a84b3c
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-05-10 23:55:52 +0200

    Ajoute une méthode addPeople

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use App\Entity\People;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +25,24 @@ class IndexController extends AbstractController
         return new JsonResponse([
             'message' => 'Welcome to your new controller!',
             'peoples' => $payload,
+        ]);
+    }
+
+    #[Route('/people/add', name: 'app_add_people', methods: ['POST'])]
+    public function addPeople(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $postData = $request->toArray();
+
+        $vip = new People();
+        $vip->setName($postData['name']);
+
+        $em = $doctrine->getManager();
+        $em->persist($vip);
+        $em->flush();
+
+        return new JsonResponse([
+            'message' => 'Welcome to app_add_people!',
+            'name' => $postData['name'],
         ]);
     }
 }
